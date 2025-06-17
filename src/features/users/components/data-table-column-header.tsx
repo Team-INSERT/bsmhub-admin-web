123fc5507d0eddf1d0f6fae77766c9f475b30d58
jaehwan02 <jhrick0129@gmail.com>
2025-06-18 00:58:16 +0900

    fix : 내림차순 오름차순 수정

--- src/features/users/components/data-table-column-header.tsx
+++ src/features/users/components/data-table-column-header.tsx
@@ -52,11 +52,11 @@ export function DataTableColumnHeader<TData, TValue>({
         <DropdownMenuContent align='start'>
           <DropdownMenuItem onClick={() => column.toggleSorting(false)}>
             <ArrowUpIcon className='mr-2 h-3.5 w-3.5 text-muted-foreground/70' />
-            내림차순
+            오름차순
           </DropdownMenuItem>
           <DropdownMenuItem onClick={() => column.toggleSorting(true)}>
             <ArrowDownIcon className='mr-2 h-3.5 w-3.5 text-muted-foreground/70' />
-            오름차순
+            내림차순
           </DropdownMenuItem>
           {column.getCanHide() && (
             <>
