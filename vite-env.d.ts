ab22d86fa3546926a911d60bf07baca427b5a4e2
Obtuse triangle <me@obtuse.kr>
2025-07-20 16:22:24 +0900

    [I25-145] 개발용 env 주소 추가

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -5,6 +5,8 @@ interface ImportMetaEnv {
   readonly VITE_PUBLIC_SUPABASE_ANON_KEY: string;
   readonly VITE_PUBLIC_GOOGLE_CLIENT_ID: string;
   readonly VITE_PUBLIC_SITE_URL: string;
+  readonly VITE_DEV_PUBLIC_SUPABASE_URL: string;
+  readonly VITE_DEV_PUBLIC_SUPABASE_ANON_KEY: string;
 }
 
 interface ImportMeta {
